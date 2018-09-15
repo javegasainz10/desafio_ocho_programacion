8f8175bf2c7e08ab170b3c23ee67e65bc8263834
Jose Vega Sainz <javegasainz@gmail.com>
2018-09-15 19:45:50 -0300

    nueva version

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 checkElement = document.getElementById('object');
 
 function check() {
-    if (checkElement.value === '' || null || undefined) {
-        checkElement.classList.remove('is-invalid');
+    if (checkElement.value === '' || null || undefined || checkElement.value.indexOf('@') < 1 || checkElement.value.indexOf('.') < 2) {
+        checkElement.classList.remove('is-valid');
         checkElement.classList.add('is-invalid');
     } else {
         checkElement.classList.remove('is-invalid');
